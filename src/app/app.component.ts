51730a2b8b948ab4532d4f6011f02fafdf3149cb
prince <prince.bc@mitrahsoft.com>
2025-04-11 19:11:51 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'princeQrCodeGenerator';
+  public url: string = 'https://prince-javascript-compiler.vercel.app/';
+  public myAngularxQrCode: string = this.url;
+  public qrCodeDownloadLink: SafeUrl = '';
+  public selectedLevel: any = 'Q';
+  selectedLogo: any = ''
+
+  public qrLevels = [
+    { name: 'Q', percent: 25, image: 'assets/level-q.png' },
+    { name: 'H', percent: 30, image: 'assets/level-h.png' },
+    { name: 'M', percent: 15, image: 'assets/level-m.png' },
+    { name: 'L', percent: 7, image: 'assets/level-l.png' }
+  ];
+
+  public logoList = [
+    { image: 'assets/logonone.png' },
+    { image: 'assets/logo1.png' },
+    { image: 'assets/logo2.png' },
+    { image: 'assets/logo3.png' },
+    { image: 'assets/logo4.png' },
+    { image: 'assets/logo5.png' },
+    { image: 'assets/logo6.png' },
+    { image: 'assets/logo7.png' },
+    { image: 'assets/logo8.png' },
+    { image: 'assets/logo9.png' },
+    { image: 'assets/logo10.png' },
+    { image: 'assets/logo11.png' },
+  ]
+
+  onChangeURL(url: SafeUrl) {
+    if (this.selectedLogo) {
+      const canvas: any = document.querySelector('canvas');
+      if (!canvas) return;
+      const ctx = canvas.getContext('2d');
+      const logo = new Image();
+      logo.crossOrigin = 'anonymous';
+      logo.src = this.selectedLogo;
+      logo.onload = () => {
+        const canvasSize = canvas.width;
+        const logoSize = 30;
+        const x = (canvasSize - logoSize) / 2;
+        const y = (canvasSize - logoSize) / 2;
+        ctx.drawImage(logo, x, y, logoSize, logoSize);
+        const updatedUrl = canvas.toDataURL('image/png');
+        this.qrCodeDownloadLink = updatedUrl;
+      };
+    } else {
+      this.qrCodeDownloadLink = url;
+    }
+  }
+
+  changeUrl(url: string) {
+    this.myAngularxQrCode = url;
+  }
+
+  selectLevel(level: string) {
+    this.selectedLevel = level;
+  }
+
+  clickLogo(logo: any) {
+    logo === 'assets/logonone.png' ? this.selectedLogo = "" : this.selectedLogo = logo
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QRCodeModule,
+    FormsModule    ,
   ],
   providers: [],
   bootstrap: [AppComponent]
